38ef9785aebd564e857350f6b6e7595db5f40350
Suellen Colangelo <suellen.cola@gmail.com>
2021-09-28 21:57:07 -0300

    Updating viewModel to call the getRandomUseCase and update the view

--- app/src/main/java/br/com/suellencolangelo/rabbits/ui/random/RandomRabbitViewModel.kt
+++ app/src/main/java/br/com/suellencolangelo/rabbits/ui/random/RandomRabbitViewModel.kt
@@ -1,10 +1,36 @@
 package br.com.suellencolangelo.rabbits.ui.random
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.suellencolangelo.rabbits.domain.usecase.GetRandomRabbitUseCase
+import br.com.suellencolangelo.rabbits.ui.mapper.RabbitModelToUiModelMapper
+import br.com.suellencolangelo.rabbits.ui.model.RabbitUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RandomRabbitViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class RandomRabbitViewModel @Inject constructor(
+    private val getRandomRabbitUseCase: GetRandomRabbitUseCase,
+    private val rabbitMapper: RabbitModelToUiModelMapper,
+) : ViewModel() {
+    private val _state = MutableLiveData<UiState>().apply { value = UiState.Loading }
+    val state: LiveData<UiState> = _state
+
+    fun getRandomRabbit() = viewModelScope.launch {
+        _state.value = UiState.Loading
+        getRandomRabbitUseCase.invoke().onSuccess {
+            _state.value = UiState.Success(rabbitMapper.mapTo(it))
+        }.onFailure {
+            _state.value = UiState.Error
+        }
+    }
+}
+
+sealed class UiState {
+    object Loading : UiState()
+    object Error : UiState()
+    data class Success(val rabbit: RabbitUiModel) : UiState()
 }
